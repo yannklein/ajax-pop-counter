244597f5c0e0fb90c4d360a79b6ab936f9887bc3
Yann Klein <yann.klein@me.com>
2022-08-24 01:57:47 +0900

    create country seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Destroy countries"
+Country.destroy_all
+
+puts "Create countries"
+Country.create!(
+  name: "France",
+  pop_growth: 1.5,
+  flag: "🇫🇷"
+)
+
+Country.create!(
+  name: "New Zealand",
+  pop_growth: 1.3,
+  flag: "🇳🇿"
+)
+
+Country.create!(
+  name: "India",
+  pop_growth: 6,
+  flag: "🇮🇳"
+)
+
+Country.create!(
+  name: "Japan",
+  pop_growth: 0.3,
+  flag: "🇯🇵"
+)
+
+Country.create!(
+  name: "Nigeria",
+  pop_growth: 10,
+  flag: "🇳🇬"
+)
+
+puts "done"
